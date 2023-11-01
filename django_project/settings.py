5fcb2c91ff27577778bc3d093d819d1f070c0a5d
LudoTreb <l.trebalag@gmail.com>
2023-11-01 17:55:02 +0100

    Config the settings.py for postgres database.

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oand9$tz&+n!$^l_l)z)=^*0!(-t(*scocmhn+x*v01q+e1eqo'
+SECRET_KEY = (
+    'django-insecure-oand9$tz&+n!$^l_l)z)=^*0!(-t(*scocmhn+x*v01q+e1eqo'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,9 +76,13 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 
