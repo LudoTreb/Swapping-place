b688d9539d10ee0d09a944a3f8d0485b94eab0d4
LudoTreb <l.trebalag@gmail.com>
2023-11-11 10:59:51 +0100

    add get_absolute_url to the SwappingProduct model

--- swapping/models.py
+++ swapping/models.py
@@ -1,53 +1,54 @@
 from django.db import models
+from django.urls import reverse
 
 
 class SwappingProduct(models.Model):
     class CategoryChoices(models.TextChoices):
-        clothing = 'Clothing', 'clothing'
-        shoes = 'Shoes', 'shoes'
+        clothing = "Clothing", "clothing"
+        shoes = "Shoes", "shoes"
 
     class SexChoices(models.TextChoices):
-        women = 'Women', 'women'
-        man = 'Man', 'man'
-        kid = 'Kid', 'kid'
+        women = "Women", "women"
+        man = "Man", "man"
+        kid = "Kid", "kid"
 
     class SizeChoices(models.TextChoices):
-        xs = 'XS', 'xs'
-        s = 'S', 's'
-        m = 'M', 'm'
-        l = 'L', 'l'
-        xl = 'XL', 'xl'
-        xxl = 'XXL', 'xxl'
+        xs = "XS", "xs"
+        s = "S", "s"
+        m = "M", "m"
+        l = "L", "l"
+        xl = "XL", "xl"
+        xxl = "XXL", "xxl"
 
     class ProductConditionChoices(models.TextChoices):
-        new = 'New', 'new'
-        as_new = 'As new', 'as new'
-        very_good_condition = 'Very good condition', 'very good condition'
-        good_condition = 'Good condition', 'good condition'
-        fair_condition = 'Fair condition', 'fair condition'
-        worn = 'Worn', 'worn'
+        new = "New", "new"
+        as_new = "As new", "as new"
+        very_good_condition = "Very good condition", "very good condition"
+        good_condition = "Good condition", "good condition"
+        fair_condition = "Fair condition", "fair condition"
+        worn = "Worn", "worn"
 
     class ColorChoices(models.TextChoices):
-        black = 'Black', 'black'
-        white = 'White', 'white'
-        red = 'Red', 'red'
-        green = 'Green', 'green'
-        blue = 'Blue', 'blue'
-        yellow = 'Yellow', 'yellow'
-        orange = 'Orange', 'orange'
-        pink = 'Pink', 'pink'
-        purple = 'Purple', 'purple'
-        brown = 'Brown', 'brown'
-        silver = 'Silver', 'silver'
-        gold = 'Gold', 'gold'
+        black = "Black", "black"
+        white = "White", "white"
+        red = "Red", "red"
+        green = "Green", "green"
+        blue = "Blue", "blue"
+        yellow = "Yellow", "yellow"
+        orange = "Orange", "orange"
+        pink = "Pink", "pink"
+        purple = "Purple", "purple"
+        brown = "Brown", "brown"
+        silver = "Silver", "silver"
+        gold = "Gold", "gold"
 
     class QualityChoices(models.TextChoices):
-        poor = 'Poor', 'poor'
-        low_quality = 'Low-quality', 'low-quality'
-        good = 'Good', 'good'
-        excellent = 'Excellent', 'excellent'
-        luxurious = 'Luxurious', 'luxurious'
-        custom_made = 'Custom-made', 'custom-made'
+        poor = "Poor", "poor"
+        low_quality = "Low-quality", "low-quality"
+        good = "Good", "good"
+        excellent = "Excellent", "excellent"
+        luxurious = "Luxurious", "luxurious"
+        custom_made = "Custom-made", "custom-made"
 
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=1000)
@@ -62,3 +63,6 @@ class SwappingProduct(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("swapping_product_detail", args=[str(self.id)])
