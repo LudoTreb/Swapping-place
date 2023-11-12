9689b531e11a43b4194f291a94cc44744314079c
LudoTreb <l.trebalag@gmail.com>
2023-11-12 10:18:48 +0100

    create 'update', 'delete' and 'create' views, config their urls and create their templates

--- swapping/urls.py
+++ swapping/urls.py
@@ -1,10 +1,31 @@
 from django.urls import path
-from .views import SwappingProductListView, SwappingProductDetailView
+from .views import (
+    SwappingProductListView,
+    SwappingProductDetailView,
+    SwappingProductUpdateView,
+    SwappingProductDeleteView,
+    SwappingProductCreationView,
+)
 
 
 urlpatterns = [
-    path("", SwappingProductListView.as_view(), name="swapping_product_list"),
     path(
         "<int:pk>/", SwappingProductDetailView.as_view(), name="swapping_product_detail"
     ),
+    path(
+        "<int:pk>/edit/",
+        SwappingProductUpdateView.as_view(),
+        name="swapping_product_edit",
+    ),
+    path(
+        "<int:pk>/delete/",
+        SwappingProductDeleteView.as_view(),
+        name="swapping_product_delete",
+    ),
+    path(
+        "new/",
+        SwappingProductCreationView.as_view(),
+        name="swapping_product_new",
+    ),
+    path("", SwappingProductListView.as_view(), name="swapping_product_list"),
 ]
--- swapping/views.py
+++ swapping/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from .models import SwappingProduct
+from django.urls import reverse_lazy
+from .forms import SwappingProductCreationForm
 
 
 class SwappingProductListView(ListView):
@@ -12,3 +15,31 @@ class SwappingProductDetailView(DetailView):
     model = SwappingProduct
     context_object_name = "swapping_product"
     template_name = "swapping/swapping_product_detail.html"
+
+
+class SwappingProductUpdateView(UpdateView):
+    model = SwappingProduct
+    fields = (
+        "title",
+        "category",
+        "sex",
+        "size",
+        "color",
+        "description",
+        "product_condition",
+        "quality",
+    )
+    template_name = "swapping/swapping_product_edit.html"
+
+
+class SwappingProductDeleteView(DeleteView):
+    model = SwappingProduct
+    context_object_name = "swapping_product"
+    template_name = "swapping/swapping_product_delete.html"
+    success_url = reverse_lazy("swapping_product_list")
+
+
+class SwappingProductCreationView(CreateView):
+    model = SwappingProduct
+    form_class = SwappingProductCreationForm
+    template_name = "swapping/swapping_product_new.html"
