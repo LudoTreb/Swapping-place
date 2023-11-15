af37b88689db027d3b4fc59c2efbc5333ed31485
LudoTreb <l.trebalag@gmail.com>
2023-11-15 17:47:26 +0100

    add some field to the model: id with uuid, image, is_favorite, is_reserve, owner and created_date. Change the name of the 'product_condition' to just 'condition'

--- swapping/models.py
+++ swapping/models.py
@@ -1,8 +1,15 @@
+import uuid
 from django.db import models
 from django.urls import reverse
+from django.conf import settings
 
 
 class SwappingProduct(models.Model):
+    """
+    Model representing a clothing
+    item that the user will add to his store
+    """
+
     class CategoryChoices(models.TextChoices):
         clothing = "Clothing", "clothing"
         shoes = "Shoes", "shoes"
@@ -20,7 +27,7 @@ class SwappingProduct(models.Model):
         xl = "XL", "xl"
         xxl = "XXL", "xxl"
 
-    class ProductConditionChoices(models.TextChoices):
+    class ConditionChoices(models.TextChoices):
         new = "New", "new"
         as_new = "As new", "as new"
         very_good_condition = "Very good condition", "very good condition"
@@ -50,16 +57,27 @@ class SwappingProduct(models.Model):
         luxurious = "Luxurious", "luxurious"
         custom_made = "Custom-made", "custom-made"
 
-    title = models.CharField(max_length=100)
-    description = models.TextField(max_length=1000)
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=500)
     category = models.CharField(max_length=20, choices=CategoryChoices.choices)
     sex = models.CharField(max_length=8, choices=SexChoices.choices)
     size = models.CharField(max_length=3, choices=SizeChoices.choices)
-    product_condition = models.CharField(
-        max_length=25, choices=ProductConditionChoices.choices
-    )
+    condition = models.CharField(max_length=25, choices=ConditionChoices.choices)
     color = models.CharField(max_length=20, choices=ColorChoices.choices)
     quality = models.CharField(max_length=20, choices=QualityChoices.choices)
+    is_favorite = models.BooleanField(default=False)
+    is_reserved = models.BooleanField(default=False)
+    image = models.ImageField(upload_to="images_article/")
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
