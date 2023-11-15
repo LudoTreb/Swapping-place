7b30fff4eed4117841767eafbb4531b26c65898f
LudoTreb <l.trebalag@gmail.com>
2023-11-15 17:44:57 +0100

    config urls for the static media_url & root

--- django_project/urls.py
+++ django_project/urls.py
@@ -14,16 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     # Django admin
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     # User management
     path("accounts/", include("django.contrib.auth.urls")),
     # Local apps
     path("accounts/", include("accounts.urls")),
     path("swapping/", include("swapping.urls")),
     path("", include("pages.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
