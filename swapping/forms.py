6f30bf51e0a112338081d6496f17358d11a3b44f
LudoTreb <l.trebalag@gmail.com>
2023-11-18 16:49:17 +0100

    remove the field 'owner'

--- swapping/forms.py
+++ swapping/forms.py
@@ -15,7 +15,6 @@ class ProductCreationForm(forms.ModelForm):
             "condition",
             "quality",
             "description",
-            "owner",
         )
         widgets = {
             "title": forms.TextInput(
