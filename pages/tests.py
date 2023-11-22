c383000e2ed308984e8bb3411a1022551016b2ab
LudoTreb <l.trebalag@gmail.com>
2023-11-22 16:38:36 +0100

    add some tests for the how it work page

--- pages/tests.py
+++ pages/tests.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, HowItWorksPageView
 
 
 class HomepageTest(SimpleTestCase):
@@ -46,3 +46,25 @@ class AboutpageTest(SimpleTestCase):
     def test_about_pageurl_resolves_aboutpageview(self):
         view = resolve("/about/")
         self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
+
+
+class HowpageTest(SimpleTestCase):
+    def setUp(self):
+        url = reverse("how")
+        self.response = self.client.get(url)
+
+    def test_url_at_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_howpage_template(self):
+        self.assertTemplateUsed(self.response, "how.html")
+
+    def test_howpage_contains_correct_html(self):
+        self.assertContains(self.response, "How does it work?")
+
+    def test_howpage_does_not_contains_incorrect_html(self):
+        self.assertNotContains(self.response, "I'm not suppose to be here")
+
+    def test_how_pageurl_resolves_howpageview(self):
+        view = resolve("/how/")
+        self.assertEqual(view.func.__name__, HowItWorksPageView.as_view().__name__)
