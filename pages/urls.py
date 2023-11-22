01c1a9195c16b8ed017271d4736fb533c96ec151
LudoTreb <l.trebalag@gmail.com>
2023-11-22 16:39:01 +0100

    Create and config url for the view and template 'how it works' page.

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, HowItWorksPageView
 
 urlpatterns = [
     path("about/", AboutPageView.as_view(), name="about"),
+    path("how/", HowItWorksPageView.as_view(), name="how"),
     path("", HomePageView.as_view(), name="home"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -7,3 +7,7 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
+
+
+class HowItWorksPageView(TemplateView):
+    template_name = "how.html"
